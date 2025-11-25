d5ce3bb1c46bc5e1e0a6a3f99bd427f3c57f728a
masgor10 <bosokersgaming@gmail.com>
2025-11-25 23:56:24 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("tombol").addEventListener("click", function () {
+    document.getElementById("judul").textContent = "Tombol Berhasil Diklik!";
+    alert("Halo! Kamu baru saja klik tombol 👍");
+});
